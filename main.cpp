a50ec290d1c343ebd4aafbd213f7282bc2358091
Mohamed Thabet <thabetx@gmail.com>
2016-04-24 21:18:32 +0200

    Tweak tests

--- main.cpp
+++ main.cpp
@@ -6,59 +6,59 @@
 using namespace std;
 
 int main(){
-  FirstFitMemory *memory = new FirstFitMemory();
+  FirstFitMemory *memFirst = new FirstFitMemory();
   
-  memory->allocateMemory(1,1024);
+  memFirst->allocateMemory(1,1024);
+  memFirst->allocateMemory(5,6);
+  memFirst->deallocateMemory(1);
   
-  memory->allocateMemory(2,5);
-  memory->allocateMemory(3,5);
-  memory->allocateMemory(4,5);
-  memory->allocateMemory(5,5);
+  memFirst->allocateMemory(2,2);
+  memFirst->allocateMemory(3,3);
+  memFirst->allocateMemory(4,4);
+  memFirst->allocateMemory(5,5);
 
-  memory->deallocateMemory(2);
-  memory->deallocateMemory(4);
-  memory->deallocateMemory(3);
+  memFirst->deallocateMemory(2);
+  memFirst->deallocateMemory(4);
+  memFirst->deallocateMemory(3);
 
-  memory->allocateMemory(6, 3);
+  memFirst->allocateMemory(6, 3);
   
-  memory->print();
-
+  memFirst->print();
 
   BestFitMemory *memBest = new BestFitMemory;
 
   memBest->allocateMemory(1, 1024);
-  memBest->allocateMemory(2, 5);
-  memBest->allocateMemory(3, 5);
-  memBest->allocateMemory(4, 5);
-  memBest->allocateMemory(5, 5);
+  memBest->deallocateMemory(1);
+
+  memBest->allocateMemory(2,2);
+  memBest->allocateMemory(3,3);
+  memBest->allocateMemory(4,4);
+  memBest->allocateMemory(5,5);
 
   memBest->deallocateMemory(2);
   memBest->deallocateMemory(4);
   memBest->deallocateMemory(3);
 
   memBest->allocateMemory(6, 3);
-  /*
 
-  */
   memBest->print();
 
   WorstFitMemory *memWorst = new WorstFitMemory;
-  memWorst->allocateMemory(4, 1024);
-  memWorst->allocateMemory(1, 5);
-  memWorst->allocateMemory(2, 5);
-  memWorst->allocateMemory(3, 5);
 
-  memWorst->allocateMemory(5, 5);
+  memWorst->allocateMemory(1,1024);
+  memWorst->allocateMemory(5,6);
+  memWorst->deallocateMemory(1);
+
+  memWorst->allocateMemory(2,2);
+  memWorst->allocateMemory(3,3);
+  memWorst->allocateMemory(4,4);
+  memWorst->allocateMemory(5,5);
 
   memWorst->deallocateMemory(2);
   memWorst->deallocateMemory(4);
-  /*
   memWorst->deallocateMemory(3);
 
   memWorst->allocateMemory(6, 3);
-  */
 
   memWorst->print();
-
-
 }
