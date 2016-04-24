2bd0c4d419cf331bd90d91359c85cf86474d6851
Amr Mohamed <amr_mohamed@live.com>
2016-04-24 16:24:55 +0200

    Implement the worst fit technique

--- WorstFitMemory.cpp
+++ WorstFitMemory.cpp
@@ -1,8 +1,37 @@
 #include "WorstFitMemory.h"
+#include "Process.h"
 bool WorstFitMemory::allocateMemory(int processID , int processSize)
 {
-	for(int blockIndex=0;blockIndex<memoryLocations.size();blockIndex++)
+	std::list<Block *>::iterator worstLocation=memoryLocations.end();
+	for(std::list<Block *>::iterator blk=memoryLocations.begin();blk!=memoryLocations.end();blk++)
 	{
-		//check if block has id 
+		if(!(*blk)->isOccupied())
+		{
+			if((*blk)->getLimit()>=processSize)
+			{
+				if(worstLocation==memoryLocations.end())
+					worstLocation=blk;
+				else if((*blk)->getLimit()>(*worstLocation)->getLimit())
+				{
+					worstLocation=blk;
+				}
+			}
+			
+		}
 	}
+	
+	if(worstLocation!=memoryLocations.end())
+	{
+		Process * p=new Process(processID,(*worstLocation)->getBase(),processSize);
+		(*worstLocation)->setBase((*worstLocation)->getBase()+processSize);
+		(*worstLocation)->setLimit((*worstLocation)->getLimit()-processSize);
+		memoryLocations.insert(worstLocation,p);
+		worstLocation++;
+		if((*worstLocation)->getLimit()==0)
+		{
+			memoryLocations.erase(worstLocation);
+		}
+		return true;
+	}
+	return false;
 }
